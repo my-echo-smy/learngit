34580965ef666a639fb545211ea61942b86c5ce8
simengyuan <2441241367@qq.com>
2018-02-09 15:44:20 +0800

    修改

--- demo1/src/test/java/com/example/demo/CityCodeTest.java
+++ demo1/src/test/java/com/example/demo/CityCodeTest.java
@@ -38,7 +38,7 @@ public class CityCodeTest {
     @Test
     public void cityTest() {
         CityCode cityCode = cityCodeService.getCityCode("云F");
-        System.out.println("转换后的json数据为" + new Gson().toJson(cityCode));
+        System.out.println("转换后的jsonwwwwwwwwwwwwwwwwwwwwwwwwwwww数据为" + new Gson().toJson(cityCode));
     }
 
 
